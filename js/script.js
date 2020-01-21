007598d30461b5fa183223c0d967b0437d83f5e1
Edward <edwrdshen@gmail.com>
2020-01-21 20:53:38 +1100

    List Name User Interface

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+window.onload = function () {
+    // Initialize list section:
+    if (!localStorage.getItem('nameLists') == false) {
+        // @precondition: Local Storage Exists, there are list names stored in local storage:
+        addListDivider();
+        nameList = JSON.parse(localStorage.getItem('nameLists'));
+        for (i = 0; i < nameList.length; i++) {
+            let currentName = nameList[i];
+            addListDOM(currentName);
+        }
+    }
+    initializeIcon();
+
+    let dropDownRef = document.getElementById('navbarDropdown');
+    dropDownRef.addEventListener('click', function clickDropDownMenu() {
+        let dropDownMenuRef = document.querySelector('.dropdown-menu');
+        if ((dropDownMenuRef.classList.value).includes('show')) {
+            dropDownMenuRef.classList.remove('show');
+        }
+        else {
+            dropDownMenuRef.classList.add('show');
+        }
+    });
+}
+
 function newDefaultDate() {
     taskDateRef = document.getElementById('task-date');
     taskTimeRef = document.getElementById('task-time');
@@ -45,13 +70,14 @@ function newDefaultDate() {
         }
         return checkString;
     }
-    // Check if last day month;
+    // Check if last day month.
     function isLastDay(dt) {
         var test = new Date(dt.getTime()),
             month = test.getMonth();
         test.setDate(test.getDate() + 1);
         return test.getMonth() !== month;
     }
+    // Only used to check if last day of year.
     function isLastMonth(dt) {
         if ((dt.getMonth() + 1) == 12) {
             return true;
@@ -60,25 +86,113 @@ function newDefaultDate() {
     }
 }
 
+function addListDivider() {
+    newDivider = document.createElement('div');
+    newDivider.classList.add('dropdown-divider');
+    newDivider.setAttribute('id', 'secondDropDivider');
+    document.getElementById('listSection').appendChild(newDivider);
+}
+function addListDOM(listName) {
+    listSectionRef = document.getElementById('listSection');
+    let newListItem = document.createElement('a');
+    newListItem.innerHTML = listName;
+    newListItem.classList.add('dropdown-item');
+    newListItem.classList.add('nameListItem');
+    newListItem.href = '#';
+
+    // HighLight Event Listeners.
+    // Get the currently entered field name and add it to the list & append new item to the HTML DOM.
+    let newListName = document.getElementById('newListField');
+    newListName.addEventListener('keydown', function () {
+        // Enter pressed and the value of the input is equivalent to the new list item's innerHTML property.
+        if ((event.which == 13 || event.keyCode == 13) && (newListName.value == newListItem.innerHTML)) {
+            newListItem.classList.add('bg-danger');
+            newListItem.classList.add('text-light');
+        }
+    })
+    newListName.addEventListener('keyup', function () {
+        newListItem.classList.remove('bg-danger');
+        newListItem.classList.remove('text-light');
+    })
+
+    // Add TRASH ICON: <i class="far fa-trash-alt"></i>
+    let newIcon = document.createElement('i');
+    newIcon.classList.add('far')
+    newIcon.classList.add('fa-trash-alt')
+    newListItem.appendChild(newIcon);
+
+    // Append List Name Onto List Section DOM.
+    listSectionRef.appendChild(newListItem);
+}
+
+function initializeIcon() {
+    let initiateEditRef = document.getElementById('edit-names');
+    let cancelEditRef = document.getElementById('edit-names-cancel');
+
+    cancelEditRef.classList.add('hide');
+
+    initiateEditRef.addEventListener('click', function () {
+        cancelEditRef.classList.remove('hide');
+        initiateEditRef.classList.add('hide');
+    })
+    cancelEditRef.addEventListener('click', function () {
+        cancelEditRef.classList.add('hide');
+        initiateEditRef.classList.remove('hide');
+    })
+    let editIcon = document.getElementById('edit-names');
+    editIcon.addEventListener('click', function () {
+    })
+}
+
 // Initialize Page with Default Date/Time Values Whenever New Task Needs to be added.
 let newItemButtonRef = document.getElementById('newItemButton');
 newItemButtonRef.addEventListener('click', newDefaultDate);
 
+// Initiates callback function whenever the a key is pressed with the new list field is selected.
 document.getElementById('newListField').onkeypress = function (myEvent) {
     listSectionRef = document.getElementById('listSection');
+    // Confirm that the user pushed the enter button.
+    // @precondition: A new list name will be added every single time the enter key is fired.
     if (event.which == 13 || event.keyCode == 13) {
+        // Confirm whether the second Drop Divider has been inputted. {Going to need to do the reverse of this once delete function is included.}
         if (document.getElementById('secondDropDivider') == null) {
-            newDivider = document.createElement('div');
-            newDivider.classList.add('dropdown-divider');
-            newDivider.setAttribute('id', 'secondDropDivider');
-            document.getElementById('listSection').appendChild(newDivider);
+            addListDivider();
         }
-        let newListName = document.getElementById('newListField').value;
-        let newListItem = document.createElement('a');
-        newListItem.innerHTML = newListName;
-        newListItem.classList.add('dropdown-item')
-        newListItem.href = '#';
-        listSectionRef.appendChild(newListItem);
-        document.getElementById('newListField').value = "";
+        // Get the currently entered field name and add it to the list & append new item to the HTML DOM.
+        let newListName = document.getElementById('newListField');
+
+        // Update Local Storage With New List Item. [Web Browser specifies you must stringify all data before storing it.]
+        if (newListName.value == "" || newListName.value.replace(/ /g, '') == "") {
+            // alert('Stupid List Name');
+        }
+        else if (!localStorage.getItem('nameLists')) {
+            // @precondition: nameLists do not currently exist:
+            let newNameList = JSON.stringify([newListName.value]);
+            localStorage.setItem('nameLists', newNameList);
+            addListDOM(newListName.value);
+            document.getElementById('newListField').value = "";
+        }
+        else {
+            if (JSON.parse(localStorage.getItem('nameLists')).indexOf(newListName.value) == -1) {
+                let nameList = JSON.parse(localStorage.getItem('nameLists'));
+                nameList.push(newListName.value);
+                localStorage.setItem('nameLists', JSON.stringify(nameList));
+
+                // Add List Item onto DOM & Reset New Item Field.
+                addListDOM(newListName.value);
+                document.getElementById('newListField').value = "";
+            }
+        }
     }
 };
+
+// IMPROVEMENTS: FUNCTIONALITY
+// MODIFY&DELETE BUTTON NEXT TO ALL TASKS GIVING USER ABILITY TO FUCK AROUND WITH LIST NAMES. --- Tue
+// ADD A TASK SECTION ADDS IN NEW TASKS ELEMENTS. [Remember to showcase in day by day format by default] --- Tue
+// Allow users to sort tasks by day,week,month,year. --- Tue
+
+// IMPROVEMENTS: UI
+// SIDEBAR INSTEAD OF YOUR PIECE OF SHIT BOOTSTRAP LOOKING ASS SHIT. --- Wed
+// RESPONSIVE WEB FACTORS. --- Wed
+
+// IMRPOVEMENTS: SOUNDCLOUD API {OPTIONAL} [Never because its a fucking todo app edward u high functioning autism retard]
